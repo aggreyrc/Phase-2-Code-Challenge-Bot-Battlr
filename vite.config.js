df074a5cd2a45cbd8383fb775ac14bae9cf21f9f
aggreyrc <aggreyronoh@gmail.com>
2024-08-13 16:21:19 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: (id) => id === 'react-router-dom',
+  resolve: {
+    alias: {
+      '@': '/src',
     },
   },
 });
